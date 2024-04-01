ed1dbbbe38aeeef1b6b8a4d578e5549ff3bce358
NotTrashraD <bellie1579@gmail.com>
2024-04-01 11:43:20 +0200

    ak5cwip

---
+++ lua/weapons/cwr_bolemos_ak5c/sh_sounds.lua
@@ -0,0 +1,14 @@
+CustomizableWeaponry:addFireSound("CWR_AK5C_FIRE", "weapons/bolemos/ak5c/fire_fp.wav", 1, 115, CHAN_WEAPON)
+CustomizableWeaponry:addFireSound("CWR_AK5C_SIL", "weapons/bolemos/ak5c/fire_sup.wav", 1, 90, CHAN_WEAPON)
+
+
+
+CustomizableWeaponry:addReloadSound("CWR_AK5C_MAGOUT", "weapons/bolemos/ak5c/magout.wav")
+CustomizableWeaponry:addReloadSound("CWR_AK5C_MAGSLAP", "weapons/bolemos/ak5c/magslap.wav")
+CustomizableWeaponry:addReloadSound("CWR_AK5C_MAGIN", "weapons/bolemos/ak5c/magin.wav")
+CustomizableWeaponry:addReloadSound("CWR_AK5C_BOLTCATCH", "weapons/bolemos/ak5c/boltfwd.wav")
+
+
+CustomizableWeaponry:addReloadSound("CWR_AK5C_DROP", "weapons/bolemos/ak5c/drop.wav")
+CustomizableWeaponry:addReloadSound("CWR_AK5C_RAISE", "weapons/bolemos/ak5c/raise.wav")
+
